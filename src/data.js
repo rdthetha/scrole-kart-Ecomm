8e59aab39fe40fa9836f502592c364a4763f8261
rdthetha <rd10072000@gmail.com>
2022-04-08 18:24:54 +0530

    initial commit

---
+++ src/data.js
@@ -0,0 +1,84 @@
+import img1 from './image/run-shoes'
+import img2 from './image/run-shoes-2'
+import img3 from './image/cricket-shoe'
+import img4 from './image/cricket-shoe-2'
+import img5 from './image/indoor-shoe'
+import img6 from './image/indoor-shoe-2'
+
+export const data = [
+    {
+      id: 1,
+      img: img1,
+      productBrand: "V1 Running Shoes",
+      productDetail: "Cricket shoes with excellent grip and cushioning!",
+      originalPrice: 5000,
+      price: 4000,
+      discount: 20,
+      ratings: 4.7,
+      inStock: true,
+      fastDelivery: false
+    },
+    {
+      id: 2,
+      img: img2,
+      productBrand: "S2 Running Shoes",
+      productDetail: "Shoes with high stabilty!",
+      originalPrice: 6000,
+      price: 5400,
+      discount: 10,
+      ratings: 4.1,
+      inStock: false,
+      fastDelivery: true
+    },
+    {
+      id: 3,
+      img: img3,
+      productBrand: "A1 Cricket shoes",
+      productDetail: "Performance in the game!",
+      originalPrice: 4000,
+      price: 3600,
+      discount: 10,
+      ratings: 4.9,
+      inStock: true,
+      fastDelivery: false
+    }
+    ,
+    {
+      id: 4,
+      img: img4,
+      productBrand: "A7 Cricket Shoes",
+      productDetail: "Stabilty and Versatile!",
+      originalPrice: 6900,
+      price: 5000,
+      discount: 20,
+      ratings: 4.4,
+      inStock: false,
+      fastDelivery: true
+    }
+    ,
+    {
+      id: 5,
+      img: img5,
+      productBrand: "E4 Indoor Shoes",
+      productDetail: "Great indoor performance!",
+      originalPrice: 6000,
+      price: 4800,
+      discount: 20,
+      ratings: 4.9,
+      inStock: true,
+      fastDelivery: true
+    }
+    ,
+    {
+      id: 6,
+      img: img6,
+      productBrand: "M10 Indoor Shoes",
+      productDetail: "Great for indoor activities!",
+      originalPrice: 3000,
+      price: 2700,
+      discount: 10,
+      ratings: 4.1,
+      inStock: true,
+      fastDelivery: false
+    }
+]
